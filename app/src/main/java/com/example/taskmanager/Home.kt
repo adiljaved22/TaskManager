f7ce2c05564b9b0d39f37646102067a8910f478d
adil488444 <engadiljavedbhutta@gmail.com>
2025-08-20 20:05:48 +0500

    half adding database for registration

---
+++ app/src/main/java/com/example/taskmanager/Home.kt
@@ -0,0 +1,77 @@
+package com.example.taskmanager
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.rememberAsyncImagePainter
+
+
+@Composable
+fun Bio() {
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ) {
+
+        val user = UserSession.currentUser
+
+        if (user != null) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Image(
+                    painter = rememberAsyncImagePainter(user.profileImage),
+                    contentDescription = "Profile Image",
+                    modifier = Modifier.size(60.dp).clip(CircleShape),
+                    contentScale = ContentScale.Crop
+                )
+
+                Spacer(modifier = Modifier.width(12.dp))
+                Column {
+                    Text(text = user.username)
+                }
+            }
+        }
+
+
+        FloatingActionButton(
+            onClick = { },
+            containerColor = Color.Black,
+            contentColor = Color.White,
+            modifier = Modifier
+                .align(Alignment.BottomEnd)
+                .padding(16.dp)
+        ) {
+            Icon(
+                imageVector = Icons.Default.Add,
+                contentDescription = "Add"
+            )
+        }
+    }
+}
---
+++ app/src/main/java/com/example/taskmanager/data/DataBase.kt
@@ -0,0 +1,3 @@
+package com.example.taskmanager.data
+
+data class DataBase()
